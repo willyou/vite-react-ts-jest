413eb497a9d0bdd3f290047424c8cb4b85fd0cdd
Will <hi@itsgookit.com>
2024-03-26 15:56:02 +0800

    feat: implment mock login in App.tsx & add logo image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
 import "./App.css";
+import { LoginForm } from "./components/LoginForm";
 
 function App() {
-  return <></>;
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  // Mock the login process.
+  const login = () => {
+    return new Promise((resolve, _) => {
+      setTimeout(() => {
+        resolve("Login Success!");
+      }, 1000);
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://mywave.ai" target="_blank">
+          <img
+            src={
+              "https://mywave.ai/wp-content/uploads/2022/10/mywave-logo-white.svg"
+            }
+            className="logo"
+            alt="Company logo"
+          />
+        </a>
+      </div>
+      <LoginForm
+        hidden={isLoggedIn}
+        onSubmit={async () => {
+          await login();
+          setIsLoggedIn(true);
+        }}
+      />
+      <div>{isLoggedIn ? <p>Welcome to MyWAVE.AI</p> : null}</div>
+    </>
+  );
 }
 
 export default App;
